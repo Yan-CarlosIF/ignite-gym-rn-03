308da69a649b9ba958dcf318821df85304fa5d39
Yan-CarlosIF <yan312005@gmail.com>
2025-09-01 16:04:16 -0300

    feat: new app theme, loading component and SignIn page image

--- App.tsx
+++ App.tsx
@@ -1,32 +1,25 @@
+import { Loading } from "@components/Loading";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { StatusBar, Text, View } from "react-native";
+import { SignIn } from "@screens/SignIn";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
+import { THEME } from "src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Text>Hello World!</Text>
-    </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
